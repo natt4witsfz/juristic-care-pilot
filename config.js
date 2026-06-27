d867be227afbb90ecf5b38de0eaa131abf21127b
natt4witsfz <natt4witsfz@users.noreply.github.com>
2026-06-27 16:58:32 +0700

    Deploy Juristic Care pilot web app

---
+++ config.js
@@ -0,0 +1,4 @@
+window.JURISTIC_CONFIG = {
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzqmodGvGgl7MPJIq7HgGBKItgXGOD7Ofw_j7WrqK_pP55MihdQY2owYzygctWWkZvo/exec",
+  REMOTE_SYNC: true
+};
